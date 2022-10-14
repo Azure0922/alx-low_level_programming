c75872381ec312575f8ee04c24591b7d7d0e46e1
Azure0922 <maryam44keks@gmail.com>
2022-10-14 10:47:03 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print comb
+ *
+ * Return: 0, success
+ */
+int main(void)
+{
+	int x = '0';
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+		if (x < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
