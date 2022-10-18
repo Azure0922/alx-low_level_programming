3cf8ddd480ecfecaa3da2093ca5169b64185f67c
Azure0922 <maryam44keks@gmail.com>
2022-10-18 03:26:11 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int _putchar(char c);
+
+/**
+ * main - print
+ *
+ * _putchar - prototype
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
