932236370a0b38a57ca8076a2ba18b599973c256
Azure0922 <maryam44keks@gmail.com>
2022-10-27 14:32:26 -0700

    1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -20,23 +20,23 @@ char *leet(char *c)
 
 	for (i = 0; i < l; i++)
 	{
-		if (c[i] = 'a' || c[i] = 'A')
+		if (c[i] == 'a' || c[i] == 'A')
 		{
 			c[i] = '4';
 		}
-		else if (c[i] = 'e' || c[i] = 'E')
+		else if (c[i] == 'e' || c[i] == 'E')
 		{
 			c[i] = '3';
 		}
-		else if (c[i] = 'o' || c[i] = 'O')
+		else if (c[i] == 'o' || c[i] == 'O')
 		{
 			c[i] = '0';
 		}
-		else if (c[i] = 'l' || c[i] = 'L')
+		else if (c[i] == 'l' || c[i] == 'L')
 		{
 			c[i] = '1';
 		}
-		else if (c[i] = 't' || c[i] = 'T')
+		else if (c[i] == 't' || c[i] == 'T')
 		{
 			c[i] = '7';
 		}
