f31d289b8138966764a3bc05314f5e5260617498
Azure0922 <maryam44keks@gmail.com>
2022-11-04 10:13:14 -0700

    staticlibraries

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+int _atoi(char *s)
+{
+	int i = strlen(s);
+
+	return (i);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+
+int _strlen(char *s)
+{
+	int l = strlen(s);
+
+	return (l);
+}
