c93d16fc81a47f2d8f04bf7651f855f7847474a7
Azure0922 <maryam44keks@gmail.com>
2022-11-04 13:33:28 -0700

    add

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,68 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ *
+ * @argc: count
+ * @argv: vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	else
+	{
+		int i, r;
+		/**
+		 * i - atoi
+		 * r - rem
+		 */
+
+		i = atoi(argv[1]);
+		r = i % 25;
+
+		if (r == 0)
+		{
+			printf("%d\n", i / 25);
+		}
+		else
+		{
+			int j;
+			/* j - rem 2 */
+
+			j = r % 10;
+
+			if (j == 0)
+			{
+				printf("%d\n", (i / 25) + (r / 10));
+			}
+			else
+			{
+				int k;
+				/* k - rem 3 */
+
+				k = j % 5;
+
+				if (k == 0)
+				{
+					printf("%d\n", (i / 25) + (r / 10) + (j / 5));
+				}
+				else
+				{
+					int l;
+					/* l - rem */
+
+					l = k % 2;
+
+					printf("%d\n", l);
+				}
+			}
+		}
+	}	
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ *
+ * @argc: count
+ * @argv: vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc = 1)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
+	{
+		if (isdigit(argv[]))
+		{
+			int i, j = 0;
+			/**
+			 * i - int
+			 * j - sum
+			 */
+
+			for (i = 0; i < argc; i++)
+			{
+				j += atoi(argv[i]);
+			}
+			printf("%d", j);
+		}
+		else
+		{
+			printf("ERROR");
+
+			return (1);
+		}
+	}
+	return (0);
+}
