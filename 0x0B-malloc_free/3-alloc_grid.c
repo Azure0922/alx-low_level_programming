652d8ec31ed53396554e5e01abcd99f2eef53f73
Azure0922 <maryam44keks@gmail.com>
2022-11-08 05:05:22 -0800

    alloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function
+ *
+ * @width: int
+ * @height: int
+ * Return: 0
+ */
+int **alloc_grid(int width, int height)
+{
+	width = 0;
+	height = 0;
+
+	if ((width <= 0) || (height <= 0))
+	{
+		return (NULL);
+	}
+	else
+		return (0);
+}
